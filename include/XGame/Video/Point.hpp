b8b731faaceb3dd9e3b66b4bee9c6d70bd884ab2
neo333 <neo333_email@yahoo.it>
2013-11-20 01:08:45 +0100

    Point conforme allo std exception c++11

--- include/XGame/Video/Point.hpp
+++ include/XGame/Video/Point.hpp
@@ -9,34 +9,34 @@ namespace xgame{
 	class XGAME_API_VIDEO Point{
 	public:
 		//! Costruttore di default
-		inline Point(const int x =0, const int y =0);
+		inline Point(const int x =0, const int y =0) throw();
 		
 		//! \return		La componente X del punto.
-		inline int Get_X_Component() const;
+		inline int Get_X_Component() const throw();
 
 		//! \return		La componente Y del punto.
-		inline int Get_Y_Component() const;
+		inline int Get_Y_Component() const throw();
 
 		//! \param [in] x	Setta la componente X del punto.
-		inline void Set_X_Component(const int x);
+		inline void Set_X_Component(const int x) throw();
 
 		//! \param [in] y	Setta la componente Y del punto.
-		inline void Set_Y_Component(const int y);
+		inline void Set_Y_Component(const int y) throw();
 
 		//! \return		La somma di due punti
-		inline Point operator+(const Point& oth) const;
+		inline Point operator+(const Point& oth) const throw();
 
 		//! Aggiunge un punto a questo punto
-		inline Point& operator+=(const Point& oth);
+		inline Point& operator+=(const Point& oth) throw();
 
 		//! \return		La differenze di due punti
-		inline Point operator-(const Point& oth) const;
+		inline Point operator-(const Point& oth) const throw();
 
 		//! Sottrae un punto a questo punto
-		inline Point& operator-=(const Point& oth);
+		inline Point& operator-=(const Point& oth) throw();
 
 		//! \return		Il modulo del punto
-		inline double Get_Module() const;
+		inline double Get_Module() const throw();
 
 		//! Casting in SDL_point
 		inline operator SDL_Point&();
@@ -49,36 +49,36 @@ namespace xgame{
 	};
 
 
-	inline Point::Point(const int x, const int y){
+	inline Point::Point(const int x, const int y) throw(){
 		m_data.x=x;
 		m_data.y=y;
 	}
 
-	inline int Point::Get_X_Component() const{ return this->m_data.x; }
+	inline int Point::Get_X_Component() const throw(){ return this->m_data.x; }
 
-	inline int Point::Get_Y_Component() const{ return this->m_data.y; }
+	inline int Point::Get_Y_Component() const throw(){ return this->m_data.y; }
 
-	inline void Point::Set_X_Component(const int x){ this->m_data.x = x; }
+	inline void Point::Set_X_Component(const int x)throw(){ this->m_data.x = x; }
 
-	inline void Point::Set_Y_Component(const int y){ this->m_data.y = y; }
+	inline void Point::Set_Y_Component(const int y)throw(){ this->m_data.y = y; }
 
-	inline Point Point::operator+(const Point& oth) const{ return Point(this->m_data.x + oth.m_data.x, this->m_data.y + oth.m_data.y); }
+	inline Point Point::operator+(const Point& oth) const throw(){ return Point(this->m_data.x + oth.m_data.x, this->m_data.y + oth.m_data.y); }
 
-	inline Point& Point::operator+=(const Point& oth){
+	inline Point& Point::operator+=(const Point& oth) throw(){
 		this->m_data.x+=oth.m_data.x;
 		this->m_data.y+=oth.m_data.y;
 		return *this;
 	}
 
-	inline Point Point::operator-(const Point& oth) const{ return Point(this->m_data.x - oth.m_data.x, this->m_data.y - oth.m_data.y); }
+	inline Point Point::operator-(const Point& oth) const throw(){ return Point(this->m_data.x - oth.m_data.x, this->m_data.y - oth.m_data.y); }
 
-	inline Point& Point::operator-=(const Point& oth){
+	inline Point& Point::operator-=(const Point& oth) throw(){
 		this->m_data.x-=oth.m_data.x;
 		this->m_data.y-=oth.m_data.y;
 		return *this;
 	}
 
-	inline double Point::Get_Module() const{ return sqrt(static_cast<double>(m_data.x*m_data.x) + static_cast<double>(m_data.y*m_data.y)); }
+	inline double Point::Get_Module() const throw(){ return sqrt(static_cast<double>(m_data.x*m_data.x) + static_cast<double>(m_data.y*m_data.y)); }
 
 	inline Point::operator SDL_Point&(){
 		return m_data;
